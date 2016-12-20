6bcdd9cb255f21238af80755937c8f74ec116b54
Alex Mason <Alex.Mason@data61.csiro.au>
2016-12-20 11:51:40 +1100

    Catch upload errors and abort, code cleanup

--- src/Network/AWS/S3/StreamingUpload.hs
+++ src/Network/AWS/S3/StreamingUpload.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE BangPatterns          #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE PatternGuards         #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TemplateHaskell       #-}
 
 module Network.AWS.S3.StreamingUpload where
 
-import           Network.AWS
+import           Network.AWS                            (AsError (..),
+                                                         HasEnv (..),
+                                                         LogLevel (..),
+                                                         MonadAWS, send, toBody)
 import           Network.AWS.S3.AbortMultipartUpload
 import           Network.AWS.S3.CompleteMultipartUpload
 import           Network.AWS.S3.CreateMultipartUpload
-import           Network.AWS.S3.ListMultipartUploads
-import           Network.AWS.S3.ListParts
 import           Network.AWS.S3.Types                   (BucketName,
                                                          CompletedPart,
                                                          ObjectKey, cmuParts,
@@ -20,6 +21,7 @@ import           Network.AWS.S3.Types                   (BucketName,
 import           Network.AWS.S3.UploadPart
 
 import           Control.Monad                          (when)
+import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 import           Control.Monad.Morph
 import           Control.Monad.Reader.Class
@@ -28,96 +30,75 @@ import           Control.Monad.Trans.Resource
 import           Data.Conduit
 import           Data.Conduit.Binary                    (sourceFile)
 
-import           Control.Applicative
-import           Data.ByteString                        as BS
+import           Data.ByteString                        (ByteString)
+import qualified Data.ByteString                        as BS
 import           Data.ByteString.Builder
-import qualified Data.ByteString.Lazy                   as BSL
-import           Data.Foldable                          (asum)
 import           Data.Monoid                            ((<>))
 -- import           Data.Text                              (Text)
 
+import           Control.Exception.Lens
 import           Control.Lens
-import           Control.Monad.Error.Class
-import           Control.Monad.Error.Lens
 
 import           Data.List.NonEmpty                     as NE (NonEmpty,
                                                                nonEmpty)
 
-data StreamingUploadError
-  = FailedToCreateUpload CreateMultipartUpload CreateMultipartUploadResponse
-  | PartUploadFailure UploadPartResponse
-  deriving (Show, Eq)
-
-makeClassyPrisms ''StreamingUploadError
 
 streamUpload :: (MonadResource m, MonadAWS m, HasEnv r, MonadReader r m) -- , AsStreamingUploadError e, MonadError e m)
              => CreateMultipartUpload
              -> Sink ByteString m CompleteMultipartUploadResponse
 streamUpload cmu = do
   logger <- lift $ view envLogger
+  let logStr :: MonadIO m => String -> m ()
+      logStr = liftIO . logger Info . stringUtf8
+
   cmur <- lift (send cmu)
-  if cmur ^. cmursResponseStatus /= 200
-    -- then throwing _FailedToCreateUpload (cmu,cmur)
-    then fail "Failed to create upload"
-    else do
-      liftIO $ logger Info "\n**** Created upload"
-      let Just upId = cmur ^. cmursUploadId
-          bucket = cmu ^. cmuBucket
-          key    = cmu ^. cmuKey
-          partUploader = uploadPart bucket key
-          logStr :: MonadIO m => String -> m ()
-          logStr = liftIO . logger Info . stringUtf8
-          -- checkUpload :: (AsStreamingUploadError e, MonadError e m)
-          --             => UploadPartResponse -> m ()
-          -- checkUpload upr = when (upr ^. uprsResponseStatus /= 200)
-          --                       $ throwing _PartUploadFailure upr
-          checkUpload :: Monad m => UploadPartResponse -> m UploadPartResponse
-          checkUpload upr = do
-            when (upr ^. uprsResponseStatus /= 200)
-                $ fail "Failed to upload piece"
-            pure upr
-          -- go :: Text -> Builder -> Int -> Int -> Sink ByteString m ()
-          go !bu !bufsize !partnum !completed = do
-              mbs <- Data.Conduit.await
-              case mbs of
-                Nothing -> lift $ do
-                            rs <- bu
-                                    & toLazyByteString
-                                    & toBody
-                                    & partUploader partnum upId
-                                    & send
-                                    >>= checkUpload
-                            liftIO $ logger Info $ stringUtf8 $ Prelude.concat ["\n**** Uploaded (final) part ",show partnum," size ",show bufsize,"\n"]
-                            let
-                              parts :: Maybe (NonEmpty CompletedPart)
-                              parts = do
-                                  cs <- sequence ((completedPart partnum <$> (rs ^. uprsETag)) : completed)
-                                  NE.nonEmpty . Prelude.reverse $ cs
+  when (cmur ^. cmursResponseStatus /= 200) $
+    fail "Failed to create upload"
 
-                            send $ completeMultipartUpload bucket key upId
-                                    & cMultipartUpload ?~ (completedMultipartUpload & cmuParts .~ parts)
+  logStr "\n**** Created upload\n"
+
+  let Just upId = cmur ^. cmursUploadId
+      bucket    = cmu  ^. cmuBucket
+      key       = cmu  ^. cmuKey
+      -- go :: Text -> Builder -> Int -> Int -> Sink ByteString m ()
+      go !bu !bufsize !partnum !completed = Data.Conduit.await >>= \mbs -> case mbs of
+        Just bs | l <- BS.length bs
+                , bufsize + l <= 6*1024*1024 -> -- Making this 5MB+1 seemed to cause AWS to complain
+                    go (bu <> byteString bs) (bufsize + l) partnum completed
+
+                | otherwise -> do
+                    rs <- lift $ checkUpload =<< partUploader partnum (bu <> byteString bs)
+
+                    logStr $ concat ["\n**** Uploaded part ", show partnum
+                                    ," size: ", show bufsize,"\n"]
+                    go mempty 0 (partnum+1) $ (completedPart partnum <$> (rs ^. uprsETag)) : completed
+
+        Nothing -> lift $ do
+            rs <- checkUpload =<< partUploader partnum bu
+
+            logStr $ concat ["\n**** Uploaded (final) part ", show partnum
+                            ," size: ", show bufsize,"\n"]
+
+            let allParts = (completedPart partnum <$> (rs ^. uprsETag)) : completed
+                -- Parts must be in ascending order
+                prts = NE.nonEmpty =<< reverse <$> sequence allParts
+
+            send $ completeMultipartUpload bucket key upId
+                    & cMultipartUpload ?~ set cmuParts prts completedMultipartUpload
 
 
-                Just bs | l <- BS.length bs, bufsize + l <= (6*1024^2) ->
-                            go (bu <> byteString bs) (bufsize + l) partnum completed
-                        | otherwise -> do
-                            rs <- lift $ bs
-                                          & byteString
-                                          & mappend bu
-                                          & toLazyByteString
-                                          & toBody
-                                          & partUploader partnum upId
-                                          & send
-                                          >>= checkUpload
-                            liftIO $ logger Info $ stringUtf8 $ Prelude.concat ["\n**** Uploaded part ",show partnum," size ",show bufsize,"\n"]
-                            go mempty 0 (partnum+1) $ (completedPart partnum <$> (rs ^. uprsETag)) : completed
+      partUploader :: MonadAWS m => Int -> Builder -> m UploadPartResponse
+      partUploader pnum = send . uploadPart bucket key pnum upId . toBody . toLazyByteString
 
-      go (mempty :: Builder) 0 1 []
-      --  `catches`
-      --   [handler _StreamingUploadError $ \e -> do
-      --       rs <- lift $ send (abortMultipartUpload bucket key upId)
-      --       pure (Left rs)
-      --   ] -- TODO AbortMultipartUpload if this fails
+      checkUpload :: Monad m => UploadPartResponse -> m UploadPartResponse
+      checkUpload upr = do
+        when (upr ^. uprsResponseStatus /= 200) $ fail "Failed to upload piece"
+        pure upr
+
+  catching _Error (go mempty 0 1 []) $ \e -> do
+      lift $ send (abortMultipartUpload bucket key upId)
+      throwM e
+  -- TODO: AbortMultipartUpload if this fails
 
 
 uploadFile :: (MonadAWS m, HasEnv r, MonadReader r m, MonadResource m)
@@ -125,3 +106,5 @@ uploadFile :: (MonadAWS m, HasEnv r, MonadReader r m, MonadResource m)
            -> m CompleteMultipartUploadResponse
 uploadFile bucket key filepath =
   sourceFile filepath $$ streamUpload (createMultipartUpload bucket key)
+
+-- TODO: This can be more efficient/concurrent if we know we're reading from a file
