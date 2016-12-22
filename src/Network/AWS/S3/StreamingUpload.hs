a057fc062894798fd0bfeb547695af21fd735d21
Alex Mason <Alex.Mason@data61.csiro.au>
2016-12-22 19:51:39 +1100

    Implement concurrent uploading, fixes #1 (or what it became…)

--- src/Network/AWS/S3/StreamingUpload.hs
+++ src/Network/AWS/S3/StreamingUpload.hs
@@ -2,15 +2,24 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ParallelListComp      #-}
 {-# LANGUAGE PatternGuards         #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 
-module Network.AWS.S3.StreamingUpload where
+module Network.AWS.S3.StreamingUpload
+(
+    streamUpload
+    , UploadLocation(..)
+    , concurrentUpload
+    , module Network.AWS.S3.CreateMultipartUpload
+    , module Network.AWS.S3.CompleteMultipartUpload
+) where
 
 import           Network.AWS                            (HasEnv (..),
                                                          LogLevel (..),
-                                                         MonadAWS, hashedBody,
-                                                         send, toBody)
+                                                         MonadAWS, getFileSize,
+                                                         hashedBody, send,
+                                                         toBody)
 
 import           Control.Monad.Trans.AWS                (AWSConstraint)
 import           Network.AWS.Data.Crypto                (Digest, SHA256,
@@ -38,6 +47,8 @@ import           Data.Conduit.List                      (sourceList)
 import           Data.ByteString                        (ByteString)
 import qualified Data.ByteString                        as BS
 import           Data.ByteString.Builder
+import           System.IO.MMap                         (mmapFileByteString)
+
 import qualified Data.DList                             as D
 import           Data.List                              (unfoldr)
 import           Data.List.NonEmpty                     (nonEmpty)
@@ -46,6 +57,8 @@ import           Control.Exception.Lens
 import           Control.Lens
 
 import           Text.Printf                            (printf)
+
+import           Control.Concurrent.Async.Lifted        (forConcurrently)
 
 -- | Minimum size of data which will be sent in a single part, currently 6MB
 chunkSize :: Int
@@ -116,3 +129,72 @@ streamUpload cmu = do
       -- Whatever happens, we abort the upload and rethrow
 
 
+-- | Specifies whether to upload a file or 'ByteString
+data UploadLocation
+    = FP FilePath -- ^ A file to be uploaded
+    | BS ByteString -- ^ A strict 'ByteString'
+
+ -- | IO (Int -> IO (Maybe ByteString)) (Either (IO ()) (IO ()))
+        -- part number as input, may be called many times until Nothing is returned,
+        -- and a function to close either this part or all parts
+
+{-|
+Allows a file or 'ByteString' to be uploaded concurrently, using the
+async library.  'ByteString's are split into 'chunkSize' chunks
+and uploaded directly.
+
+Files are mmapped into 'chunkSize' chunks and each chunk is uploaded in parallel.
+This considerably reduces the memory necessary compared to reading the contents
+into memory as a strict 'ByteString'. The usual caveats about mmaped files apply:
+if the file is modified during this operation, the data become corrupted.
+
+May throw `Error`, or `IOError`.
+-}
+concurrentUpload :: (MonadAWS m, MonadBaseControl IO m)
+                 => UploadLocation -> CreateMultipartUpload -> m CompleteMultipartUploadResponse
+concurrentUpload ud cmu = do
+    cmur <- send cmu
+    when (cmur ^. cmursResponseStatus /= 200) $
+        fail "Failed to create upload"
+    let Just upId = cmur ^. cmursUploadId
+        bucket    = cmu  ^. cmuBucket
+        key       = cmu  ^. cmuKey
+        -- hndlr :: SomeException -> m CompleteMultipartUploadResponse
+        hndlr e = send (abortMultipartUpload bucket key upId) >> throwM e
+
+    handling id hndlr $ do
+        umrs <- case ud of
+            BS bs -> forConcurrently (zip [1..] $ chunksOf chunkSize bs) $ \(partnum, b) -> do
+                    umr <- send . uploadPart bucket key partnum upId . toBody $ b
+                    pure $ completedPart partnum <$> (umr ^. uprsETag)
+
+            FP fp -> do
+                fsize <- liftIO $ getFileSize fp
+                let (count,lst) = divMod (fromIntegral fsize) chunkSize
+                    params = [(partnum, chunkSize*offset, size)
+                            | partnum <- [1..]
+                            | offset  <- [0..count]
+                            | size    <- (chunkSize <$ [0..count-1]) ++ [lst]
+                            ]
+
+                forConcurrently params $ \(partnum,off,size) -> do
+                    b <- liftIO $ mmapFileByteString fp (Just (fromIntegral off,size))
+                    umr <- send . uploadPart bucket key partnum upId . toBody $ b
+                    pure $ completedPart partnum <$> (umr ^. uprsETag)
+
+        let prts = nonEmpty =<< sequence umrs
+        send $ completeMultipartUpload bucket key upId
+                & cMultipartUpload ?~ set cmuParts prts completedMultipartUpload
+
+
+
+
+-- http://stackoverflow.com/questions/32826539/chunksof-analog-for-bytestring
+justWhen :: (a -> Bool) -> (a -> b) -> a -> Maybe b
+justWhen f g a = if f a then Just (g a) else Nothing
+
+nothingWhen :: (a -> Bool) -> (a -> b) -> a -> Maybe b
+nothingWhen f = justWhen (not . f)
+
+chunksOf :: Int -> BS.ByteString -> [BS.ByteString]
+chunksOf x = unfoldr (nothingWhen BS.null (BS.splitAt x))
