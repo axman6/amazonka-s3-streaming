4408e580c9aa8fc8d0a71b9b28538d3cf64e138b
Alex Mason <Axman6@gmail.com>
2022-06-29 20:20:32 +1000

    Simplify s3upload executable

--- Main.hs
+++ Main.hs
@@ -9,63 +9,54 @@ import Data.Conduit                         ( runConduit, (.|) )
 import Data.Conduit.Binary                  ( sourceHandle )
 import Data.Functor                         ( (<&>) )
 import Data.Text                            ( pack )
-import Network.AWS
-       ( AWS, Credentials(FromFile), HasEnv(envRegion), newEnv, runAWS, runResourceT )
-import Network.AWS.Data.Text                ( fromText )
-import Network.AWS.S3.CreateMultipartUpload ( createMultipartUpload )
+
 import Network.AWS.S3.StreamingUpload
        ( UploadLocation(FP), abortAllUploads, concurrentUpload, streamUpload )
+
+import Amazonka (runResourceT)
+import Amazonka.Env (newEnv, envRegion)
+import Amazonka.Auth (discover)
+import Amazonka.S3.Types (BucketName(..), ObjectKey (..))
+import Amazonka.S3.CreateMultipartUpload    ( newCreateMultipartUpload )
 
 import Control.Monad.IO.Class ( liftIO )
 import System.Environment     ( getArgs )
 import System.IO              ( BufferMode(BlockBuffering), hSetBuffering, stdin )
 
-#if !MIN_VERSION_base(4,8,0)
-import Control.Applicative ( pure, (<$>), (<*>) )
-#endif
-
 main :: IO ()
 main = do
   args <- getArgs
+
+  env <- newEnv discover
+
   case args of
-    (region:profile:credfile:bucket:key:file:_) ->
-      case (,,,) <$> (FromFile <$> fromText (pack profile) <*> pure credfile)
-                 <*> fromText (pack region)
-                 <*> fromText (pack bucket)
-                 <*> fromText (pack key)
-      of
-        Right (creds,reg,buck,ky) -> do
-          env <- newEnv creds <&> set envRegion reg
-          hSetBuffering stdin (BlockBuffering Nothing)
-          res <- runResourceT . runAWS env $ case file of
-                  "-" -> runConduit (sourceHandle stdin .| streamUpload @AWS Nothing (createMultipartUpload buck ky))
-                          >>= liftIO . either print print
-                  _   -> concurrentUpload Nothing Nothing (FP file) (createMultipartUpload buck ky)
-                          >>= liftIO . print
+    ("upload":bucket:key:file:_) -> do
+        let buck = BucketName $ pack bucket
+            ky   = ObjectKey $ pack key
+        hSetBuffering stdin (BlockBuffering Nothing)
+        res <- runResourceT $ case file of
+                "-" -> runConduit (sourceHandle stdin .| streamUpload env Nothing (newCreateMultipartUpload buck ky))
+                        >>= liftIO . either print print
+                _   -> concurrentUpload env Nothing Nothing (FP file) (newCreateMultipartUpload buck ky)
+                        >>= liftIO . print
 
-          print res
-        Left err -> print err >> usage
-    ("abort":region:profile:credfile:bucket:_) ->
-      case (,,) <$> (FromFile <$> fromText (pack profile) <*> pure credfile)
-                <*> fromText (pack region)
-                <*> fromText (pack bucket)
-      of
-        Right (creds,reg,buck) -> do
-          env <- newEnv creds <&> set envRegion reg
-          res <- runResourceT . runAWS env . abortAllUploads $ buck
-          print res
-        Left err -> print err >> usage
+        print res
 
+    ("abort":region:profile:credfile:bucket:_) -> do
+          res <- runResourceT $ abortAllUploads env (BucketName $ pack bucket)
+          print res
     _ -> usage
 
 usage :: IO ()
 usage = putStrLn . unlines $
-  [ "Usage: \n"
+  [ "Usage:"
+  , ""
   , "  Upload file:"
-  , "    s3upload <region:ap-southeast-2> <profile> <credentials file:$HOME/.aws/credentials> <bucket> <object key> <file to upload>"
+  , "    s3upload upload <bucket> <object key> <file to upload>"
+  , ""
   , "  Abort all unfinished uploads for bucket:"
-  , "    s3upload abort <region:ap-southeast-2> <profile> <credentials file:$HOME/.aws/credentials> <bucket>\n"
-  , "all arguments must be supplied - the region will be obtained from the AWS_REGION env var"
-  , "if compiled with amazonka > 1.4.4, but must still be supplied (making an option parsing library"
-  , "a dependency of this package seemed overkill)"
+  , "    s3upload abort <bucket>"
+  , ""
+  , "Uses `newEnv discover` to make the Amazonka environment, so it wil look at"
+  , "appropriate env vars, or ~/.aws/credentials, etc."
  ]
